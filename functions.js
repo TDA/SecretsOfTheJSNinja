735964ca84c122290a78ff34674a7c0e3cb2df7e
Sai Chandramouli <schand31@asu.edu>
2017-06-12 20:40:02 -0700

    functional sort

---
+++ functions.js
@@ -0,0 +1,10 @@
+/**
+ * Created by schandramouli on 6/12/17.
+ */
+
+// Functional array sort
+array = [6, 5, 3, 2, 8, 10];
+array.sort(function (value1, value2) {
+  return value1 > value2;
+});
+console.log(array);
