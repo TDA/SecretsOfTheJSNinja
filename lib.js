0e61212a912d934e60c446f330988a5e54e7b3cd
Sai Chandramouli <schand31@asu.edu>
2017-06-13 20:21:08 -0700

    extracted the asserts into lib

--- lib.js
+++ lib.js
@@ -37,3 +37,28 @@ console.log(take(users1, 'cc', mail));
 console.log(take2(users1_copy, 'cc', mail));
 console.log(take2(users2, 'cc', mail));
 console.log(take(users2_copy, 'cc', mail));
+
+(function () {
+  var results;
+  this.assert = function (value, message) {
+    const li = document.createElement("li");
+    li.className = value ? "pass" : "fail";
+    li.appendChild(document.createTextNode(message));
+    results.appendChild(li);
+    if (!value) {
+      var test_group = li.parentNode.parentNode;
+      test_group.classList.remove("pass");
+      test_group.classList.add("fail");
+    }
+    return li;
+  };
+  this.test = function test(name, fn) {
+    // Get the global results context (ul)
+    results = document.getElementById("assert-results");
+    // Get the context (li) for the current `test`
+    var test_group = assert(true, name);
+    test_group.classList.add('test-group');
+    results = test_group.appendChild(document.createElement("ul"));
+    fn();
+  };
+})();
