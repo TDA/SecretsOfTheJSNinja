e4c8e8ec8ba329b47836853a5226ae52842e7b92
Sai Chandramouli <schand31@asu.edu>
2017-07-11 15:54:21 -0700

    chirp

--- functions_anonymous.js
+++ functions_anonymous.js
@@ -3,12 +3,21 @@
  */
 
 function isPalindrome(text) {
-  if (text.length <= 1) return true;
-  if (text.charAt(0) != text.charAt(text.length - 1)) return false;
-  // console.log(text.substr(1, text.length - 2));
-  console.log(text.slice(1, text.length - 1));
-  return isPalindrome(text.substr(1, text.length - 2));
+  if (text) {
+    if (text.length <= 1) return true;
+    if (text.charAt(0) != text.charAt(text.length - 1)) return false;
+    // console.log(text.substr(1, text.length - 2)); slice does what you expect, substr does not
+    console.log(text.slice(1, text.length - 1));
+    return isPalindrome(text.substr(1, text.length - 2));
+  }
+  return false;
 }
 
 console.log(isPalindrome('saipc'));
 console.log(isPalindrome('saias'));
+
+function chirp(n) {
+  return n > 1 ? chirp(n - 1) + '-chirp' : 'chirp';
+}
+
+console.log(chirp(3));
