5c2f4449ef3adaff80ec33e14c06807891b79ba9
Sai Chandramouli <schand31@asu.edu>
2017-07-17 11:42:46 -0700

    added comments

--- functions_as_objects.js
+++ functions_as_objects.js
@@ -2,18 +2,22 @@
  * Created by schandramouli on 7/16/17.
  */
 
+// This is such a beautiful example, not only does it explain the quirks of JS,
+// but also uses CS fundamentals like Hashmaps and stuff <3
 var store = {
   nextId: 1,
   cache: {},
   add: function (fn) {
     if(!fn.id) {
       fn.id = this.nextId++;
+      // this is assignment and verifying if assignment passes
       return !! (this.cache[fn.id] = fn);
     } else {
       return false;
     }
   }
 };
+
 function ninja() {
   return 1;
 }
