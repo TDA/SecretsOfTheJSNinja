0a0024e2b4bfb4c32e1a7b55e8aa8fc68b6270dd
Sai Chandramouli <schand31@asu.edu>
2017-08-03 13:04:11 -0700

    extreme cool JS, function overloading with closures

--- dist/function_overloading.js
+++ dist/function_overloading.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 function someRandom(a) {
   console.log(a);
@@ -9,6 +9,51 @@ function someRandom2(a, b) {
 }
 
 // WHOAAA, crazy cool shizz
-console.log(someRandom.length);
-console.log(someRandom2.length);
+// console.log(someRandom.length);
+// console.log(someRandom2.length);
+
+var beverages = {
+  items: ['coffee', 'tea', 'juice', 'cranberry juice', 'apple juice']
+};
+function addOrOverloadMethod(object, name, fn) {
+  console.log("I am new", fn.length);
+  var old = object[name];
+  object[name] = function () {
+    console.log(arguments);
+    if (fn.length === arguments.length) {
+      return fn.apply(this, arguments);
+    } else if (typeof old === 'function') {
+      return old.apply(this, arguments);
+    }
+  };
+  console.log(object[name]);
+}
+
+addOrOverloadMethod(beverages, 'find', function () {
+  return this.items;
+});
+
+addOrOverloadMethod(beverages, 'find', function (keyword) {
+  // exact match
+  return this.items.filter(function (item) {
+    return item.indexOf(keyword) === 0;
+  });
+});
+
+addOrOverloadMethod(beverages, 'find', function (keyword1, keyword2) {
+  return this.items.filter(function (item) {
+    return item.indexOf(keyword1) >= 0 && item.indexOf(keyword2) >= 0;
+  });
+});
+
+console.log('-------------------------------------------------');
+console.log(beverages.find());
+console.log('-------------------------------------------------');
+console.log(beverages.find('juice'));
+console.log('-------------------------------------------------');
+console.log(beverages.find('juice', 'cran'));
+console.log('-------------------------------------------------');
+console.log(beverages.find('juice'));
+console.log('-------------------------------------------------');
+console.log(beverages.find());
 //# sourceMappingURL=function_overloading.js.map
