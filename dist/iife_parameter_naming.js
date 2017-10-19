611057978f2b17260983e64896e71d123f3e992a
Sai Chandramouli <schand31@asu.edu>
2017-10-19 14:15:40 -0700

    added comments

--- dist/iife_parameter_naming.js
+++ dist/iife_parameter_naming.js
@@ -2,6 +2,8 @@
 
 var $ = 'lol';
 
+// Inside the IIFE, the $ will always refer to the passed in
+// param. This is what jQuery uses to its advantage
 (function ($) {
   console.log($);
 })('but ha!');
--- iife_parameter_naming.js
+++ iife_parameter_naming.js
@@ -2,6 +2,8 @@
 
 let $ = 'lol';
 
+// Inside the IIFE, the $ will always refer to the passed in
+// param. This is what jQuery uses to its advantage
 (function ($) {
   console.log($);
 })('but ha!');
