632e57638a63ed66d6a79d1b98f3d67d262a8510
Sai Chandramouli <schand31@asu.edu>
2017-11-03 20:29:49 -0700

    prototypes are attached, not added

--- Prototypes/basic_prototypes.js
+++ Prototypes/basic_prototypes.js
@@ -13,3 +13,10 @@ var ninja1 = Ninja();
 var ninja2 = new Ninja();
 console.log(ninja1 && ninja1.swingSword());
 console.log(ninja2.swingSword());
+
+Ninja.prototype.doubleSwing = function () {
+  return 2;
+};
+
+console.log(ninja2.doubleSwing());
+
