7d7a78d93512e8a57a4b80378ee35287544b23a0
Sai Chandramouli <schand31@asu.edu>
2017-11-23 03:41:10 -0800

    prototype gotcha 1

--- dist/Prototypes/intermediate_prototypes.js
+++ dist/Prototypes/intermediate_prototypes.js
@@ -22,4 +22,19 @@ var newNinja = new Ninja();
 lib.assertCommandLine(newNinja instanceof Ninja, 'Still a ninja');
 lib.assertCommandLine(newNinja instanceof Person, 'But also  a person');
 lib.assertCommandLine(typeof newNinja.dance === 'function', 'And can also dance like a person :O');
+
+lib.printHorizontalRule();
+// Gotcha time, never extend Object directly
+Object.prototype.keys = function () {
+  var keys = [];
+  for (var p in this) {
+    // or at least do the hasOwn check, else you will be adding to your troubles :D
+    if (this.hasOwnProperty(p)) keys.push(p);
+  }
+
+  return keys;
+};
+
+var obj = { a: 1, b: 2, c: 3 };
+lib.assertCommandLine(obj.keys().length == 3, "There are three properties in this object.");
 //# sourceMappingURL=intermediate_prototypes.js.map
