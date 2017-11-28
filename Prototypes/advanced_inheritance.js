92d2fb87edbf7b589aebaeb50edb4e762f395997
Sai Chandramouli <schand31@asu.edu>
2017-11-28 04:29:24 -0800

    lets start something real complex - inheritance

---
+++ Prototypes/advanced_inheritance.js
@@ -0,0 +1,12 @@
+// Let's simulate traditional inheritance with JS using prototypes
+// This will allow us to mimic something very close to React.createClass({x: y}); syntax.
+
+// e.g. here will be uncommented once we finish writing the basic struture
+// var Person = Object.subClass({
+//   init: function(isDancing) {
+//     this.dancing = isDancing;
+//   },
+//   dance: function() {
+//     return this.dancing;
+//   }
+// });
