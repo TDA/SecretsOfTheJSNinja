ebf6ca398750181f03c5afccb457c57e22fba685
Sai Chandramouli <schand31@asu.edu>
2018-01-22 20:13:18 +0530

    regexes refresher, unneeded lol

---
+++ Regexes/basic_regexes.js
@@ -0,0 +1,3 @@
+console.log(/test/.test("lol"));
+
+console.log(/<(\w+)>(.+?)<\/\1>/.test("<strong>whatever</strong>"));
---
+++ dist/Regexes/basic_regexes.js
@@ -0,0 +1,6 @@
+"use strict";
+
+console.log(/test/.test("lol"));
+
+console.log(/<(\w+)>(.+?)<\/\1>/.test("<strong>whatever</strong>"));
+//# sourceMappingURL=basic_regexes.js.map
